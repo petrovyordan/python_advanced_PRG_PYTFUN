a0626f2ae2efbb315c0468c57368791dd9536183
petrovyordan <98331583+petrovyordan@users.noreply.github.com>
2022-01-24 17:41:58 +0200

    Update BTCUSDT_problem_solution.py

--- BTCUSDT_problem_solution.py
+++ BTCUSDT_problem_solution.py
@@ -32,7 +32,7 @@ class BitcoinPrice():
         json_data = json_message['data'][0]
         
         # added check if time gap between records is not too long
-        if not json_data['t'] - self.timestamps_list[-1] > 1000:
+        if not json_data['t'] - self.timestamps_list[-1] > 700:
             self.prices_list.append(json_data["p"])
             self.timestamps_list.append(json_data["t"])
             self.volumes_list.append(json_data["v"])
