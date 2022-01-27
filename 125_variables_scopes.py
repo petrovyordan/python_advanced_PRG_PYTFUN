8ed874bd6c6c55950cac3b31091a994d297cb335
petrovyordan <98331583+petrovyordan@users.noreply.github.com>
2022-01-27 17:51:12 +0200

    Update 125_variables_scopes.py

--- 125_variables_scopes.py
+++ 125_variables_scopes.py
@@ -1,33 +1,50 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 24 15:37:21 2022
+Created on Mon Jan 27 17:37:21 2022
 
 @author: Yordan.Petrov
 """
 
+import datetime
+
 user_input = input("Please input a valid file and location!\n")
 
 # make sure there isn't any quot. mark left
-if '"' in user_input or '"' in user_input:
-    user_input = user_input.replace('"', '').replace("'", '')
+user_input = user_input.replace('"', '').replace("'", '')
 
 def convert_degrees(temp):
     temp = round(float(temp) * (9 / 5) + 32, 2)
     return str(temp) + 'F'
 
 try:
+    start = datetime.datetime.now()
     with open(user_input, "r") as file:
-        for line in file:
-            
-            # replace any new lines or spaces in the line
-            line = line.replace('\n', '').replace(' ', '')
-            if line[-1] == 'C':
-                with open('converted_degrees.txt', 'a') as file:
-                    value = convert_degrees(line[:-1])
-                    file.write(value)
-                    file.write('\n')
-                    print(value)
+        with open('converted_degrees.txt', 'a') as src_file:
+            with open('error_log.txt', 'a') as errors_file:
+                counter = 0
+                for line in file:          
+                    counter += 1
+                    # replace any new lines or spaces in the line
+                    line = line.replace('\n', '').replace(' ', '')
+                    try:
+                        if line[-1] == 'C':
+                            value = convert_degrees(line[:-1])
+                            src_file.write(value)
+                            src_file.write('\n')
+                    except IndexError:
+                        errors_file.write(f"IndexError at: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} on line: {counter}\n")
+                        pass
+                    except ValueError:
+                        errors_file.write(f"ValueError at: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} on line: {counter}\n")
+                        pass
+                    except:   
+                        errors_file.write(f"OtherError at: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} on line: {counter}\n")
+                        pass
 except IOError as e:
     errno, strerror = e.args
     print("I/O error({0}): {1}".format(errno,strerror))
+    pass
 
+end = datetime.datetime.now()
+
+print(end-start)
